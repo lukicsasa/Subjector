944c040a5c51a27101dcbb035cb9584bb810cb8b
lukics <lukic.sasa11@gmail.com>
2017-11-18 20:14:58 +0100

    added cert and session tables

---
+++ Subjector.Sql/dbo/Cert.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Cert]
+( 
+    [Id] INT NOT NULL IDENTITY (1,1), 
+	[UserId] INT NOT NULL,
+    [CertNumber] NVARCHAR(250) NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_Cert_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]), 
+    PRIMARY KEY ([Id])
+)
---
+++ Subjector.Sql/dbo/Session.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Session]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [LastAction] DATETIME NOT NULL, 
+    [Token] NVARCHAR(250) NULL, 
+    [Active] BIT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_Session_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
